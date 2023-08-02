7fe900b6626cb82e8f66bfb129063fe6139a3576
Akash Pardeshi <pardeshiakash99@gmail.com>
2023-08-02 14:16:24 +0530

    Trending Carousel Added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 
 HeroBanner;
 
-export const Home = () => {
+
+const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
       <div style={{ height: 1000 }}></div>
     </div>
   );
