e0c93ace6478c0c17efced59a8ae4316ad0e1376
Akash Pardeshi <pardeshiakash99@gmail.com>
2023-08-03 14:17:32 +0530

    Home Page Completed (Popular and Top Rated section added)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
-
-HeroBanner;
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 
 const Home = () => {
@@ -11,7 +11,8 @@ const Home = () => {
     <div className="homePage">
       <HeroBanner />
       <Trending />
-      <div style={{ height: 1000 }}></div>
+      <Popular />
+      <TopRated />
     </div>
   );
 };
