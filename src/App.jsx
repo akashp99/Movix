36f07d0ec83bd0ce28b58d26a957202510feb844
Akash Pardeshi <pardeshiakash99@gmail.com>
2023-08-03 13:23:04 +0530

    Genres (movie category) added only for desktop mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -21,6 +21,7 @@ function App() {
 
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
   }, []);
 
   const fetchApiConfig = () => {
@@ -37,6 +38,24 @@ function App() {
       });
   };
 
+  const genresCall = async () => {
+    let promises = []
+    let endPoints = ["tv", "movie"]
+    let allGenres = {}
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({genres}) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
   return (
   <BrowserRouter>
   <Header />
@@ -50,6 +69,6 @@ function App() {
     <Footer />
   </BrowserRouter>
   );
-}
+};
 
 export default App;
