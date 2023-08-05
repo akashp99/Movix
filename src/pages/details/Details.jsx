075fdb39070698ae506de23588abd9d6de2ab966
Akash Pardeshi <pardeshiakash99@gmail.com>
2023-08-05 13:12:17 +0530

    Top Cast And Official Videos Added in Detail Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import "./style.scss";
 import useFetch from '../../hooks/useFetch';
 import DetailsBanner from './detailsBanner/DetailsBanner';
+import Cast from './cast/Cast';
+import VideosSection from './videosSection/VideosSection';
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -12,6 +14,8 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
     </div>
   );
 };
