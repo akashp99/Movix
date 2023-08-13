ea72f536bd8432ee41eb2b1b47bcf62cbb7c14b8
Akash Pardeshi <pardeshiakash99@gmail.com>
2023-08-13 14:01:31 +0530

    Starting with home page

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -3,17 +3,13 @@ import React from "react";
 import "./style.scss";
 
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
-import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
-    const navigate = useNavigate();
     return (
         <div className="pageNotFound">
             <ContentWrapper>
-                <span className="bigText">WELCOME TO THE MOVIX</span>
-                <div className="logo" onClick={() => navigate("/")}>
-                <span className="smallText" >Click here to exploring the Movies and TV shows.</span>
-                </div>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
             </ContentWrapper>
         </div>
     );
